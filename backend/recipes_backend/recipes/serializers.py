c71d65dd62f74e2b089af6fc2b7b186b8b6a213d
eobgolts <eobgolts@gmail.com>
2023-12-07 18:08:07 +0300

    рецепты

--- backend/recipes_backend/recipes/serializers.py
+++ backend/recipes_backend/recipes/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from recipes.models import Tag
+from recipes.models import Tag, Recipe
 from rest_framework.validators import (
     UniqueTogetherValidator
 )
@@ -16,3 +16,41 @@ class TagSerializer(serializers.ModelSerializer):
                 fields=('name', 'slug', 'color')
             )
         ]
+
+
+class RecipesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'slug', 'color')
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Tag.objects.all(),
+                fields=('name', 'slug', 'color')
+            )
+        ]
+
+
+
+'''
+class Recipe(models.Model):
+    name = models.CharField(max_length=16)
+    description = models.TextField()
+    author = models.ForeignKey(
+        User, related_name='recipes',
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(
+        upload_to='cats/images/',
+        null=True,
+        default=None
+    )
+    tags = models.ManyToManyField(Tag,
+                                  through='TagRecipe')
+    ingredients = models.ManyToManyField(Ingredient,
+                                         through='RecipeIngredient')
+    time = models.IntegerField(verbose_name='Cook time')
+
+    def __str__(self):
+        return self.name
+'''
