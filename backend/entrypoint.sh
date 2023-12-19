37b5c78369b36d53ba95197812981fc393f5342d
eobgolts <eobgolts@gmail.com>
2023-12-19 14:00:23 +0300

    Docker

---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py load_ingredients
+python manage.py load_tags
+
+
+gunicorn --bind 0.0.0.0:8080 foodgram.wsgi
