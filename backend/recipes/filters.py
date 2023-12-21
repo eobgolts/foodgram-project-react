67a5c362f10d0668c0b9d5d423a8ca27776e149d
eobgolts <eobgolts@gmail.com>
2023-12-21 11:24:52 +0300

    fix

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,10 +1,17 @@
 from django_filters import rest_framework as filters
-
+from django.forms import MultipleChoiceField
 from recipes.models import Recipe
 
 
+class NonValidatingMultipleChoiceField(MultipleChoiceField):
+    def validate(self, value):
+        pass
+
+class NonValidateChoices(filters.AllValuesMultipleFilter):
+    field_class = NonValidatingMultipleChoiceField
+
 class RecipeFilter(filters.FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = NonValidateChoices(field_name='tags__slug')
     is_in_shopping_cart = filters.BooleanFilter(method='check_cart')
     is_favorited = filters.BooleanFilter(method='check_favorited')
 
